4b369eaa9810672b948d8bef755403a4809b27a3
Manideep Regonda <mregonda@iu.edu>
2024-04-16 22:20:45 -0400

    Added Editing names feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-
+import Player from "./components/player"
 function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Manideep" symbol="X" ></Player>
+          <Player initialName ="Player 2" symbol ="O"></Player>
+          
+        </ol>
+        GAME Board
+      </div>
+      LOG
+    </main>
   )
 }
 
